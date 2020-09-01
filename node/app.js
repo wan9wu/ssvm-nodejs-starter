8db806cb4655b3f596838fa0327d628fb6e4df67
wangwenzhu <bcsxmail@gmail.com>
2020-09-02 00:23:55 +0800

    创建项目

--- node/app.js
+++ node/app.js
@@ -1,4 +1,4 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const { say, jiami, jiemi } = require('../pkg/ssvm_nodejs_starter_lib.js');
 
 const http = require('http');
 const url = require('url');
@@ -7,8 +7,21 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   const queryObject = url.parse(req.url,true).query;
+  console.log(queryObject);
   if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
+    if(queryObject['operate']){
+      if(queryObject['operate']=="encrypt" && queryObject['text']){
+        res.end(jiami(queryObject['text']) + '\n');
+      };
+      if(queryObject['operate']=="decode" && queryObject['decode']&&queryObject['key']){
+        res.end(jiemi(queryObject['decode'],queryObject['key']) + '\n');
+      };
+      res.end(`加密请使用 curl  \"http://${hostname}:${port}/?operate=encrypt&text=hello\" \n \
+      解密请使用 curl \"http://${hostname}:${port}/?operate=encrypt&decode=YOURCODE&key=YOURKEY\" \n`);
+    }else{
+      res.end(`加密请使用 curl  \"http://${hostname}:${port}/?operate=encrypt&text=hello\" \n \
+      解密请使用 curl \"http://${hostname}:${port}/?operate=encrypt&decode=YOURCODE&key=YOURKEY\" \n`);
+    }
   } else {
     res.end(say(queryObject['name']) + '\n');
   }
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,60 @@ pub fn say(s: &str) -> String {
   let r = String::from("hello ");
   return r + s;
 }
+
+#[wasm_bindgen]
+pub fn jiami(s: &str) -> String {
+  println!("jiami收到参数s:{}",s);
+  let text = s;
+  let mut bytes = Vec::new();
+
+  let mut key: u8 = 0;
+  let cipher_text = loop {
+      if key == 255 {
+          break String::from("加密失败，请适当更换文本内容")
+      };
+      key = key + 1;
+      for c in text.bytes() {
+          let (b, _) = (c as u8).overflowing_add(key);
+          bytes.push(b);
+      };
+      
+      let end_text = String::from_utf8(bytes.clone());
+      let end_text = match end_text{
+          Ok(b) => {
+              break b
+          },
+          Err(e) =>{
+              e.to_string()
+          }, 
+      };
+      println!("{}key{}", end_text, key);
+  };
+  
+  format!("密文是：\"{}\"，密码是：\"{}\"。", cipher_text, key)
+}
+
+#[wasm_bindgen]
+pub fn jiemi(s: &str,key: &str) -> String {
+  println!("jiemi收到参数s:{},key:{}",s,key);
+  let text = s;
+  let mut bytes = Vec::new();
+  let key = key.parse::<u8>().unwrap();
+
+  for c in text.bytes() {
+      let (b, _) = (c as u8).overflowing_sub(key);
+      bytes.push(b);
+  };
+  
+  let cipher_text = String::from_utf8(bytes.clone());
+  let cipher_text = match cipher_text{
+      Ok(b) => b,
+      Err(e) =>{
+          e.to_string()
+      }, 
+  };
+  format!("解密结果：\"{}\"。", cipher_text)
+}
+
+
+
